3129cb9f8481a970d723d95188e9943bf246fcf7
Diego Vázquez Campos (Kaerit) <kaerit@hotmail.com>
2025-08-23 01:47:33 +0000

    toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { RollForgeClient } from './roll-forge-client';
+import { useToast } from '@/hooks/use-toast';
 
 export default function Home() {
+  useToast(); // Temporary call for debugging build process
   return <RollForgeClient />;
 }
